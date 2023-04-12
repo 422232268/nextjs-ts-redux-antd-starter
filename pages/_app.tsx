53db9a23da5cf3d866c71b3c670dc66564dc5a72
wangxuezhong <wxzxingtian@163.com>
2023-04-12 21:10:50 +0800

    feat(change store): sage change redux-thunk

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { AppProps } from 'next/app'
-import { wrapper } from 'src/redux'
+import { Provider } from 'react-redux'
+import store from '../src/redux/store'
 import Layout from 'src/components/Layout'
 import { ConfigProvider } from 'antd'
 import 'antd/dist/reset.css'
@@ -9,21 +10,23 @@ const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => (
   <ConfigProvider
     theme={{
       token: {
-        "colorPrimary": "#2a292f",
-        "colorPrimaryBorder":"red",
-        "colorInfo": "#eabc74",
-        "fontSize": 16,
-        "borderRadiusSM": 2,
-        "borderRadiusLG": 4,
-        "borderRadius": 4,
-        "wireframe": true
+        colorPrimary: '#2a292f',
+        colorPrimaryBorder: 'red',
+        colorInfo: '#eabc74',
+        fontSize: 16,
+        borderRadiusSM: 2,
+        borderRadiusLG: 4,
+        borderRadius: 4,
+        wireframe: true,
       },
     }}
   >
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   </ConfigProvider>
 )
 
-export default wrapper.withRedux(WrappedApp)
+export default WrappedApp
---
+++ src/redux/store.ts
@@ -0,0 +1,23 @@
+import { configureStore } from '@reduxjs/toolkit'
+import StuReducer from './reducer'
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  // 黑名单 不缓存的
+  blacklist:['page404']
+};
+
+const persistedReducer = persistReducer(persistConfig, StuReducer);
+
+const store = configureStore({
+    reducer:persistedReducer
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export default store
