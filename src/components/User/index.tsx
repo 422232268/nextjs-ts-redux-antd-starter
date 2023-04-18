a39d9802c8b9a4f93c58fa5d41129bae32c91598
wxz <422232268@qq.com>
2023-04-19 00:25:20 +0800

    fix(fix error): fix error

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -1,21 +1,21 @@
 import React, { FC, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { getProfileRequest } from 'src/redux/user/actions'
+// import { getProfileRequest } from 'src/redux/user/actions'
 import Profile from './Profile'
 
 const User: FC = () => {
   const user = useSelector((state: Store.RootState) => state.user)
   const dispatch = useDispatch()
 
-  useEffect(() => {
-    if (!user.profile) {
-      dispatch(getProfileRequest({ id: 1 }))
-    }
-  }, [])
+  // useEffect(() => {
+  //   if (!user.profile) {
+  //     dispatch(getProfileRequest({ id: 1 }))
+  //   }
+  // }, [])
 
   return (
-    <>
-      <Profile profile={user.profile} isLoading={user.isLoading} />
+    <>123
+      {/* <Profile profile={user.profile} isLoading={user.isLoading} /> */}
     </>
   )
 }
